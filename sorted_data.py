a8ee26bd6d9b3ee4fed06e9ed878c9d1eed2f0a7
Caroline <animals4all@gmail.com>
2015-07-04 18:13:02 -0400

    Inside 'Main' Function

--- sorted_data.py
+++ sorted_data.py
@@ -1,11 +1,15 @@
-ratings = open("scores.txt", "r")
-contents = [line.split(":") for line in ratings]
-contents = {line[0]:line[1] for line in contents}
+def main():
+	ratings = open("scores.txt", "r")
+	contents = [line.split(":") for line in ratings]
+	contents = {line[0]:line[1] for line in contents}
 
-items = contents.items()
-items.sort()
+	items = contents.items()
+	items.sort()
 
-for item in items:
-	item = list(item)
+	for item in items:
+		item = list(item)
 
-	print "Resturant '" + item[0] + "' is rated at " + item[1][:-1] + "."
+		print "Resturant '" + item[0] + "' is rated at " + item[1][:-1] + "."
+
+if "__name__" == "__main__":
+	main()
